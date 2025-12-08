31520ef144929ecde7d923b90ffc115edb036a35
saifulislam-bd <saifulislam.dev22@gmail.com>
2025-12-08 22:25:33 +0600

    role action added

---
+++ backend/actions/role.ts
@@ -0,0 +1,20 @@
+import { devLog } from "@/lib/utils";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+
+export async function getUserRole() {
+  try {
+    const { userId } = await auth();
+
+    if (!userId) {
+      return "user";
+    }
+
+    const clerkAwait = await clerkClient();
+    const clerk = await clerkAwait.users.getUser(userId);
+
+    return (clerk.privateMetadata.role as string) || "user";
+  } catch (error) {
+    devLog.error("Error getting user role:", error);
+    return "user";
+  }
+}
