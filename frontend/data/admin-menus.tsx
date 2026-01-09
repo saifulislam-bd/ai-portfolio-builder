ea5dba76cb086affe0be377f1fd596dbe4d3a24a
saifulislam-bd <saifulislam.dev22@gmail.com>
2026-01-09 22:30:46 +0600

    admin-menus data

---
+++ frontend/data/admin-menus.tsx
@@ -0,0 +1,11 @@
+import { FileText } from "lucide-react";
+
+export const menuItems = [
+  {
+    id: "templates",
+    title: "Templates",
+    description: "Manage portfolio templates and layouts",
+    href: "/admin/templates",
+    icon: FileText,
+  },
+];
